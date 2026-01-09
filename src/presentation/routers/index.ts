424fa88b27a12d4feb2ebae62cdbed947c5191e2
Layton <jihwan8807@naver.com>
2026-01-10 01:45:20 +0900

    test 폴더 삭제

--- src/presentation/routers/index.ts
+++ src/presentation/routers/index.ts
@@ -1,17 +1,11 @@
 import { Router } from 'express';
-
-import { pingRouter } from './pingRouter';
 import { eventRouter } from './tripEventRouter';
-
 import authRouter from './authRouter';
 import userRouter from './auth/userRouter';
 import tripScheduleRouter from './tripScheduleRouter';
-// import tripScheduleRouter from './tripScheduleRouter';
-// import userRouter from './auth/userRouter';
 
 const rootRouter = Router();
 
-rootRouter.use('/ping', pingRouter);
 rootRouter.use('/auth', authRouter);
 rootRouter.use('/users', userRouter);
 rootRouter.use('/trips', tripScheduleRouter);
--- src/presentation/routers/pingRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { pingController } from '../controllers/pingController';
-
-const pingRouter = Router();
-
-pingRouter.get('/', pingController);
-
-export { pingRouter };
